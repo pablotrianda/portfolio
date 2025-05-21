39e9dae394bec56dd092e8f446af7b303b79ba5c
Pablo Triandafilide <pablotrianda@gmail.com>
2025-05-20 23:43:56 -0300

    Add browser site

--- main.go
+++ main.go
@@ -17,11 +17,22 @@ func porfolio(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprint(w, constants.Page())
 	} else {
-		fmt.Fprint(w, "<h1> This site is designed to be used from the terminal. Try 'curl ptrianda.com'. </h1>")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		data, err := os.ReadFile("public/index.html")
+		if err != nil {
+			http.Error(w, "Error loading page", http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
 	}
 }
 
 func main() {
+
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/curl.txt", fs)
+
 	http.HandleFunc("/", porfolio)
 
 	err := godotenv.Load(".env")
